2d64fe3d2fbcce2b486b1d72086032dc9196c4ed
KEHhyeon <kth050213@naver.com>
2024-01-05 12:11:32 +0900

    fa

--- es6/lib/fx.js
+++ es6/lib/fx.js
@@ -3,32 +3,10 @@ const curry =
   (a, ..._) =>
     _.length ? f(a, ..._) : (..._) => f(a, ..._);
 
-const map = curry((f, iter) => {
-  let res = [];
-  iter = iter[Symbol.iterator]();
-  let cur;
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    res.push(f(a));
-  }
-
-  return res;
-});
-
 const pipe =
   (f, ...fs) =>
   (...as) =>
     go(f(...as), ...fs);
-const filter = curry((f, iter) => {
-  let res = [];
-  let cur;
-  iter = iter[Symbol.iterator]();
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    if (f(a)) res.push(a);
-  }
-  return res;
-});
 
 const reduce = curry((f, acc, iter) => {
   if (!iter) {
@@ -81,11 +59,15 @@ L.map = curry(function* (f, iter) {
     yield f(a);
   }
 });
-L.filter = curry(function* (f, iter) {
-  let cur;
-  iter = iter[Symbol.iterator]();
-  while (!(cur = iter.next()).done) {
-    const a = cur.value;
-    if (f(a)) yield a;
-  }
+L.map = curry(function* (f, iter) {
+  for (const a of iter) yield f(a);
 });
+
+const takeAll = take(Infinity);
+const map = curry(pipe(L.map, take(Infinity)));
+
+L.filter = curry(function* (f, iter) {
+  for (const a of iter) if (f(a)) yield a;
+});
+
+const filter = curry(pipe(L.filter, takeAll));
